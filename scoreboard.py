cdf557676c8960b0b094807ed6eb1ff7cb69951b
Daivik <patel.daivik8@gmail.com>
2021-03-11 12:31:58 -0500

    high score added

--- scoreboard.py
+++ scoreboard.py
@@ -4,21 +4,29 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open('data.txt') as file:
+            self.high_score = int(file.read())
         self.color('white')
         self.penup()
         self.goto(0, 265)
-        self.write(f"Score:{self.score}",align='center',font=("Courier",24,"normal"))
+        self.write(f"Score:{self.score} High Score: {self.high_score}",align='center',font=("Courier",24,"normal"))
         self.hideturtle()
 
 
     def increase(self):
         self.score+=1
         self.clear()
-        self.write(f"Score:{self.score}", align='center', font=("Arial", 24, "normal"))
+        self.write(f"Score:{self.score} High Score: {self.high_score}", align='center', font=("Arial", 24, "normal"))
 
 
-    def game_over(self):
-        print('Over')
-        self.goto(0,0)
-        self.write(f"Game Over", align='center', font=("Arial", 24, "normal"))
+
+    def reset(self):
+        if self.score>self.high_score:
+            with open('data.txt','w') as file:
+                file.write(str(self.score))
+            self.high_score = self.score
+        self.score = 0
+        self.clear()
+        self.write(f"Score:{self.score}  High Score: {self.high_score}", align='center', font=("Arial", 24, "normal"))
+
 
